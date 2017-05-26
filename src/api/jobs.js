4148eac5962c8ffea2f0bd67811a3697d8483654
rishabh dixit <rishabhdixit@rishabhs-MacBook-Pro.local>
2017-05-26 11:12:01 +0800

    fix: Ran eslint and fixed errors

--- src/api/jobs.js
+++ src/api/jobs.js
@@ -1,52 +1,50 @@
-import resource from "../lib/resource-router";
-import jobsService from "../services/jobsService";
-import config from "../../config/index";
+import resource from '../lib/resource-router';
+import jobsService from '../services/jobsService';
+import config from '../../config/index';
 
-export default ({app}) => resource({
-    id: 'job',
+export default ({ app }) => resource({
+	id: 'job',
 
     /*
      GET /api/jobs - Fetching jobs at max 10 per request
      */
-    async index({params, query}, res) {
-        const limit = config.pageLimit;
-        const page = parseInt(query.page || 1);
-        const skip = limit * (page - 1);
-        let search_criteria = {};
-        if (query && query.location)
-            search_criteria.location = query.location;
-        if (query && query.title)
-            search_criteria['$text'] = {$search: query.title};
-        const jobs = await jobsService.getJobs(app, search_criteria, limit, skip);
-        const jobsCount = await jobsService.getJobsCount(app, search_criteria);
-        let pageMetaData = {
-            size: (jobs && jobs.length) || 0,
-            pageNumber: page,
-            totalPages: Math.ceil(jobsCount / limit),
-            totalSize: jobsCount
-        };
-        let finalResponse = {
-            jobsList: jobs,
-            pageMetaData: pageMetaData
-        };
-        res.json(finalResponse);
-    },
+	async index({ params, query }, res) {
+		const limit = config.pageLimit;
+		const page = parseInt(query.page || 1, 10);
+		const skip = limit * (page - 1);
+		const searchCriteria = {};
+		if (query && query.location)			{ searchCriteria.location = query.location; }
+		if (query && query.title)			{ searchCriteria.$text = { $search: query.title }; }
+		const jobs = await jobsService.getJobs(app, searchCriteria, limit, skip);
+		const jobsCount = await jobsService.getJobsCount(app, searchCriteria);
+		const pageMetaData = {
+			size: (jobs && jobs.length) || 0,
+			pageNumber: page,
+			totalPages: Math.ceil(jobsCount / limit),
+			totalSize: jobsCount,
+		};
+		const finalResponse = {
+			jobsList: jobs,
+			pageMetaData,
+		};
+		res.json(finalResponse);
+	},
 
     /*
      GET /api/jobs/count - Fetching count of jobs in db
      */
-    async count({params, query}, res) {
-        const count = await app.models.job.count(query);
-        res.json(count);
-    },
+	async count({ params, query }, res) {
+		const count = await app.models.job.count(query);
+		res.json(count);
+	},
 
     /*
      POST /api/jobs - Create a new job in db
      */
-    async create({body}, res) {
-        const job = await app.models.job.create(body);
-        res.json(job);
-    },
+	async create({ body }, res) {
+		const job = await app.models.job.create(body);
+		res.json(job);
+	},
 
 
 });
--- src/services/jobsService.js
+++ src/services/jobsService.js
@@ -2,37 +2,37 @@
  * Created by rishabhdixit on 25/05/2017.
  */
 module.exports = {
-    getJobs: async(app, search_criteria, limit, skip) => {
-        return new Promise(function (resolve, reject) {
-            app.models.job.native(function (err, collection) {
-                if (err)
-                    reject(err);
-                else
-                    collection.find(search_criteria, {}, {
-                        'limit': limit,
-                        'skip': skip
-                    }).toArray(function (err, results) {
-                        if (err)
-                        		reject(err);
-                        else
-                            resolve(results);
-                    });
-            })
-        })
-    },
-    getJobsCount: async(app, search_criteria) => {
-        return new Promise(function (resolve, reject) {
-            app.models.job.native(function (err, collection) {
-                if (err)
-                    reject(err);
-                else
-                    collection.count(search_criteria, function (err, count) {
-                        if (err)
-                        		reject(err);
-                        else
-                            resolve(count);
-                    });
-            })
-        })
-    }
-}
+	getJobs: async (app, searchCriteria, limit, skip) => new Promise((resolve, reject) => {
+		app.models.job.native((err, collection) => {
+			if (err) {
+				reject(err);
+			}	else {
+				collection.find(searchCriteria, {}, {
+					limit,
+					skip,
+				}).toArray((error, results) => {
+					if (error) {
+						reject(error);
+					}	else {
+						resolve(results);
+					}
+				});
+			}
+		});
+	}),
+	getJobsCount: async (app, searchCriteria) => new Promise((resolve, reject) => {
+		app.models.job.native((err, collection) => {
+			if (err) {
+				reject(err);
+			}	else {
+				collection.count(searchCriteria, (error, count) => {
+					if (error) {
+						reject(error);
+					}	else {
+						resolve(count);
+					}
+				});
+			}
+		});
+	}),
+};
